bedf458796a28db9e20860e2afa224bcd3a08866
Hanumant Uphade <hanumantuphade63@gmail.com>
2025-03-07 21:40:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from "./sections/About"
 import Hero from "./sections/Hero"
 import PopularAreas from "./sections/PopularAreas"
 import Properties from "./sections/Properties"
-import Services from "./sections/services"
+import Services from "./sections/services";
 import Clients from "./sections/Clients"
 import Contact from "./sections/Contact"
 import Footer from "./components/Footer"
